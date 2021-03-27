5045d0041b9baf1514ed8cb946e695e3e08b87df
Ivan Boothe <ivan@rootwork.org>
2021-03-26 17:12:35 -0700

    Splitting templates into partials

--- index.js
+++ index.js
@@ -9,43 +9,24 @@ const Handlebars = require("handlebars");
 const path       = require('path');
 
 //
-// DEVELOPMENT LOGGING
-//
-
-/**
-* Register a debug helper for Handlebars to be able to log data or inspect data in the browser console
-*
-* Usage:
-*   {{debug someObj.data}} => logs someObj.data to the console
-*   {{debug someObj.data true}} => logs someObj.data to the console and stops at a debugger point
-*
-* Source: https://gist.github.com/elgervb/5c38c8d70870f92ef6338a291edf88e9
-*
-* @param {any} the data to log to console
-* @param {boolean} whether or not to set a breakpoint to inspect current state in debugger
-*/
-Handlebars.registerHelper( 'debug', function( data, breakpoint ) {
-    console.log(data);
-    if (breakpoint === true) {
-        debugger;
-    }
-    return '';
-});
-// use with {{log 0 this "myString" accountName}}
-// DEBUG: 0, INFO: 1, WARN: 2, ERROR: 3,
-Handlebars.registerHelper('log', Handlebars.logger.log);
-// Std level is 3, when set to 0, handlebars will log all compilation results
-Handlebars.logger.level = 3;
-
-//
 // Paths
 //
 
 const paths = {
   data: path.resolve('./data/', 'sample.yaml'),
-  template: path.resolve('./templates/', 'table.hbs'),
   styles: path.resolve('./styles/', '*.scss'),
   dist: path.resolve('./dist/', 'sample.html')
+}
+
+const templates = {
+  dir: '/templates/',
+  main: 'table.hbs',
+  partials: [
+    'caption',
+    'head',
+    'body',
+    'footer'
+  ]
 }
 
 //
@@ -76,13 +57,45 @@ Handlebars.registerHelper('columnCount', function() {
 // Handlebar compiling
 //
 
-const template = fs.readFileSync(paths.template, 'utf8');
+// Main template.
+const template = fs.readFileSync(__dirname + templates.dir + templates.main, 'utf8');
+
+// Partials.
+for(let partial of templates.partials){
+  Handlebars.registerPartial(partial, fs.readFileSync(__dirname + templates.dir + '_' + partial + '.hbs', 'utf8'));
+}
+
+// Handlebars debugging.
+
+/**
+* Register a debug helper for Handlebars to be able to log data or inspect data in the browser console
+*
+* Usage:
+*   {{debug someObj.data}} => logs someObj.data to the console
+*   {{debug someObj.data true}} => logs someObj.data to the console and stops at a debugger point
+*
+* Source: https://gist.github.com/elgervb/5c38c8d70870f92ef6338a291edf88e9
+*
+* @param {any} the data to log to console
+* @param {boolean} whether or not to set a breakpoint to inspect current state in debugger
+*/
+Handlebars.registerHelper( 'debug', function( data, breakpoint ) {
+    console.log(data);
+    if (breakpoint === true) {
+        debugger;
+    }
+    return '';
+});
+// use with {{log 0 this "myString" accountName}}
+// DEBUG: 0, INFO: 1, WARN: 2, ERROR: 3,
+Handlebars.registerHelper('log', Handlebars.logger.log);
+// Std level is 3, when set to 0, handlebars will log all compilation results
+Handlebars.logger.level = 3;
+
+// Creating HTML file.
 const format = Handlebars.compile(template, {
   strict: true
 });
 const result = format(dataJSON);
-
 fs.writeFileSync(paths.dist, result);
 console.log(`File written to ${paths.dist}`);
-
-
